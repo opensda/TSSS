7f087f00539285a14a5a07d1db1407a899dafe51
toir02 <wend8765@gmail.com>
2023-10-23 21:31:41 +0300

    added habit list api view

--- habits/views.py
+++ habits/views.py
@@ -1,7 +1,15 @@
 from rest_framework import generics
 
+from habits.paginators import HabitPaginator
+from habits.permissions import IsOwner
 from habits.serializers import HabitSerializer
 
 
 class HabitCreateAPIView(generics.CreateAPIView):
     serializer_class = HabitSerializer
+
+
+class HabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = HabitPaginator
+    permission_classes = [IsOwner,]
