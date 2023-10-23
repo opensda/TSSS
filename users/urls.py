09cd9904483c2862813a7383a3d27c2f3f25914c
toir02 <wend8765@gmail.com>
2023-10-23 15:38:22 +0300

    added paths to authorization with jwt

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.views import UserCreateAPIView
 
 urlpatterns = [
-    path('create/', UserCreateAPIView.as_view(), name='create-user')
+    path('create/', UserCreateAPIView.as_view(), name='create-user'),
+    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
