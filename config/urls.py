eb2d8b854acfd791284af3f2665fbbb8c0187202
toir02 <wend8765@gmail.com>
2023-10-23 15:21:00 +0300

    added paths to habits and users apps

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -35,6 +35,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('habits/', include('habits.urls', namespace='habits')),
+    path('users/', include('users.urls', namespace='users')),
 ]
