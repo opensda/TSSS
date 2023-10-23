290017169543f58625ab47be6317f4dfbc087f66
toir02 <wend8765@gmail.com>
2023-10-23 21:29:54 +0300

    added habit create api view

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from habits.serializers import HabitSerializer
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    serializer_class = HabitSerializer
