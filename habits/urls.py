c78978a83027a869cdd9d3ea08e1bff494d1c98e
toir02 <wend8765@gmail.com>
2023-10-23 21:53:42 +0300

    added method as_view() to controller

--- habits/urls.py
+++ habits/urls.py
@@ -4,7 +4,7 @@ from habits.views import *
 
 urlpatterns = [
     path('create/', HabitCreateAPIView.as_view(), name='habits-create'),
-    path('', HabitListAPIView(), name='habits-list'),
+    path('', HabitListAPIView().as_view(), name='habits-list'),
     path('public/', HabitPublicListAPIView.as_view(), name='habits-public-list'),
     path('update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habits-update'),
     path('delete/<int:pk>/', HabitDestroyAPIView.as_view(), name='habits-delete'),
