5f6533016d47baeb5b0a69bbdece5e8278698ede
toir02 <wend8765@gmail.com>
2023-10-23 22:06:22 +0300

    added method save to user model

--- users/models.py
+++ users/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     user_id = models.PositiveIntegerField(unique=True, verbose_name='tg id', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.set_password(self.password)
+        super().save(*args, **kwargs)
