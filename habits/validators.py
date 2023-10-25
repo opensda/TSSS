7f05f00c8b8c750ea778583e6ad3f933b9c4d15a
toir02 <wend8765@gmail.com>
2023-10-25 22:52:34 +0300

    added related habit and reward validator

--- habits/validators.py
+++ habits/validators.py
@@ -11,3 +11,16 @@ class DurationValidator:
         if field_value is not None:
             if int(field_value) > 120:
                 raise ValidationError('Время выполнения должно быть не больше 120 секунд.')
+
+
+class RelatedHabitAndRewardValidator:
+    def __init__(self, field1, field2):
+        self.field1 = field1
+        self.field2 = field2
+
+    def __call__(self, value):
+        related_habit = dict(value).get(self.field1)
+        reward = dict(value).get(self.field2)
+
+        if related_habit and reward:
+            raise ValidationError(f"Нельзя одновременно указать {self.field1} и {self.field2}.")
