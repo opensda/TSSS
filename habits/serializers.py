f8b44b6c773e4e1a5dd90c6e5c1aeabeef2d258c
toir02 <wend8765@gmail.com>
2023-10-25 22:53:43 +0300

    added related habit and reward validator to habit serializer

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 
 from habits.models import Habit
-from habits.validators import DurationValidator
+from habits.validators import *
 
 
 class HabitSerializer(serializers.ModelSerializer):
     validators = [
         DurationValidator(field='duration'),
+        RelatedHabitAndRewardValidator(field1='related_habit', field2='reward'),
     ]
 
     class Meta:
