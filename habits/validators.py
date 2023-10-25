81a671f72c06e32f8995354a3da907ee9c243691
toir02 <wend8765@gmail.com>
2023-10-25 22:58:33 +0300

    added is pleasant validator

--- habits/validators.py
+++ habits/validators.py
@@ -2,7 +2,6 @@ from rest_framework.exceptions import ValidationError
 
 
 class DurationValidator:
-
     def __init__(self, field):
         self.field = field
 
@@ -24,3 +23,18 @@ class RelatedHabitAndRewardValidator:
 
         if related_habit and reward:
             raise ValidationError(f"Нельзя одновременно указать {self.field1} и {self.field2}.")
+
+
+class IsPleasantValidator:
+    def __init__(self, field1, field2, field3):
+        self.field1 = field1
+        self.field2 = field2
+        self.field3 = field3
+
+    def __call__(self, value):
+        is_pleasant = dict(value).get(self.field1)
+        related_habit = dict(value).get(self.field2)
+        reward = dict(value).get(self.field3)
+
+        if is_pleasant and (related_habit or reward):
+            raise ValidationError("Приятная привычка не может иметь связанной привычки или вознаграждения.")
