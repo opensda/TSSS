45afd9064c5dc8e0d9288595a34c1de0c4eb9d70
toir02 <wend8765@gmail.com>
2023-10-25 23:57:35 +0300

    moved validators to class meta

--- habits/serializers.py
+++ habits/serializers.py
@@ -5,15 +5,14 @@ from habits.validators import *
 
 
 class HabitSerializer(serializers.ModelSerializer):
-    validators = [
-        DurationValidator(field='duration'),
-        RelatedHabitAndRewardValidator(field1='related_habit', field2='reward'),
-        IsPleasantValidator(field1='is_pleasant', field2='related_habit', field3='reward')
-    ]
-
     class Meta:
         model = Habit
         fields = '__all__'
         extra_kwargs = {
             'user': {'required': False}
         }
+        validators = [
+            DurationValidator(field='duration'),
+            RelatedHabitAndRewardValidator(field1='related_habit', field2='reward'),
+            IsPleasantValidator(field1='is_pleasant', field2='related_habit', field3='reward')
+        ]
