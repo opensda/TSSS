67fe15bd988680f19c609eed323d6ff2bb285f08
toir02 <wend8765@gmail.com>
2023-10-27 20:28:42 +0300

    added field telegram username to user model

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     user_id = models.PositiveIntegerField(unique=True, verbose_name='tg id', blank=True, null=True)
+    telegram_username = models.CharField(unique=True, verbose_name='telegram username', max_length=255)
 
     def save(self, *args, **kwargs):
         self.set_password(self.password)
