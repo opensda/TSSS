60bfb4138c6783572f56e82f65623ff01bbcf5b0
toir02 <wend8765@gmail.com>
2023-10-28 01:19:21 +0300

    added strftime to habit time and current time

--- habits/tasks.py
+++ habits/tasks.py
@@ -12,10 +12,10 @@ bot = TeleBot(settings.TELEGRAM_BOT_TOKEN, threaded=False)
 @shared_task
 def send_notifications():
     habits = Habit.objects.all()
-    current_time = datetime.now().time()
+    current_time = datetime.now()
 
     for habit in habits:
-        if habit.time == current_time:
+        if habit.time.strftime("%H:%M") == current_time.strftime("%H:%M"):
             if habit.user.user_id:
                 bot.send_message(chat_id=habit.user.user_id, text=f'Пришло время привычки: {habit.action}. '
                                                                   f'Привычку необходимо выполнять в {habit.place}.')
