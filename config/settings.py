a22e9697ea448d7fab966945a2c288c0e8ede85f
toir02 <wend8765@gmail.com>
2023-10-27 22:32:29 +0300

    added celery beat schedule to settings

--- config/settings.py
+++ config/settings.py
@@ -167,3 +167,10 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+CELERY_BEAT_SCHEDULE = {
+    'task-name': {
+        'task': 'habits.tasks.send_notifications',
+        'schedule': timedelta(minutes=1),
+    },
+}
