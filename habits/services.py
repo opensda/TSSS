e4c6a437756cbdd23e2a2417c04da46fea517def
toir02 <wend8765@gmail.com>
2023-10-27 23:20:25 +0300

    deleted services.py

--- habits/services.py
+++
@@ -1,9 +0,0 @@
-import requests
-from django.conf import settings
-
-TOKEN = settings.TELEGRAM_BOT_TOKEN
-
-
-def get_updates():
-    response = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
-    return response.json()
