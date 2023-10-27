7ace60860d3059b96683fd8b03c66c26351142df
toir02 <wend8765@gmail.com>
2023-10-27 17:26:29 +0300

    changed field period to positive small int field with default value 1

--- habits/models.py
+++ habits/models.py
@@ -17,7 +17,7 @@ class Habit(models.Model):
     related_habit = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name='связанная привычка', blank=True,
                                       null=True)
     is_pleasant = models.BooleanField(default=False, verbose_name='признак приятной привычки')
-    period = models.CharField(choices=PERIOD_CHOICES, default='everyday', max_length=20, verbose_name='периодичность')
+    period = models.PositiveSmallIntegerField(default=1, verbose_name='периодичность')
     reward = models.CharField(max_length=255, verbose_name='вознаграждение')
     duration = models.PositiveIntegerField(default=120, verbose_name='время на выполнение')
     is_public = models.BooleanField(default=False, verbose_name='признак публикации')
