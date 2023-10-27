385e031e8de711d6be1735463096b1ce1d60eeb3
toir02 <wend8765@gmail.com>
2023-10-28 01:59:08 +0300

    added test delete habits

--- habits/tests.py
+++ habits/tests.py
@@ -88,3 +88,10 @@ class HabitTestCase(APITestCase):
         response = self.client.put(url, data=data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_habits(self):
+        url = reverse("habits:habits-delete", args=[self.habit.id])
+
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
