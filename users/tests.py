8820a90c69a942a643a0f718ac8dedfa395dcb64
toir02 <wend8765@gmail.com>
2023-10-28 12:06:19 +0300

    added class user test case with test create user

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class UserTestCase(APITestCase):
+    def setUp(self):
+        pass
+
+    def test_create_user(self):
+        url = reverse('users:create-user')
+        data = {
+            "username": "testuser",
+            "password": "123"
+        }
+
+        response = self.client.post(url, data=data)
+
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
