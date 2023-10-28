aacbf5d1720f5d134b435151e188a8bd976d308e
toir02 <wend8765@gmail.com>
2023-10-28 12:03:03 +0300

    changed schedule timedelta to 1 minute

--- config/settings.py
+++ config/settings.py
@@ -171,6 +171,6 @@ TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 CELERY_BEAT_SCHEDULE = {
     'send-notifications': {
         'task': 'habits.tasks.send_notifications',
-        'schedule': timedelta(seconds=31),
+        'schedule': timedelta(minutes=1),
     },
 }
