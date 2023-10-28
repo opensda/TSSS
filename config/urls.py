77de422cf2fcdb3662267a473ad438d3c612c540
toir02 <wend8765@gmail.com>
2023-10-28 15:16:46 +0300

    edit description for documentation

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
         default_version='v1',
-        description="Your API description",
+        description="Documentation for API TaskTracker",
         terms_of_service="https://www.example.com/policies/terms/",
         contact=openapi.Contact(email="contact@example.com"),
         license=openapi.License(name="BSD License"),
